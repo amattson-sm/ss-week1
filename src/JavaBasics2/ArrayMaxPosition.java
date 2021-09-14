9eab8a8d954d880f1c24e6f9952a780e72811a14
aidan.mattson <aidan.mattson@wsu.edu>
2021-09-14 13:38:45 -0700

    added week 2 templates

---
+++ src/JavaBasics2/ArrayMaxPosition.java
@@ -0,0 +1,2 @@
+package Java2;public class ArrayMaxPosition {
+}
---
+++ src/JavaBasics2/CommandLineSum.java
@@ -0,0 +1,14 @@
+package Java2;
+
+/**
+ * @author aidan.mattson
+ * Java Basics 2
+ * Assignment 1: Add command line arguments
+ */
+public class CommandLineSum {
+    public static void main(String[] args) {
+        for (String in : args) {
+
+        }
+    }
+}
